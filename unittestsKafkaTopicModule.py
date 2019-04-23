f0735c46d0cee092297fc5540aee98239b470628
Johanna Koch <johanna.koch@post.ch>
2019-04-23 13:47:09 +0200

    realised to use mocks to check if func called instead of raise

--- unittestsKafkaTopicModule.py
+++ unittestsKafkaTopicModule.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import unittest
+import mock
 
 class TestValidateClass(unittest.TestCase):
     # validate_name
@@ -10,7 +11,10 @@ class TestValidateClass(unittest.TestCase):
 
     def validate_name_fail(self):
         import kafka_topic
-        self.assertRaises(Exception, kafka_topic.validate_name, name="topic:topic")
+        mo = mock.Mock()
+        mo.fail_module()
+        kafka_topic.validate_name(name="topic:topic")
+        mo.fail_module.assert_called()
 
     # validate_factor
     def validate_factor_test(self):
@@ -19,7 +23,10 @@ class TestValidateClass(unittest.TestCase):
 
     def validate_factor_fail(self).
         import kafka_topic
-        self.assertRaises(Exception, kafka_topic.validate_factor, factor=2.1, part_or_rep="partition")
+        mo = mock.Mock()
+        mo.fail_module()
+        kafka_topic.validate_factor(factor=2.1, part_or_rep="partition")
+        mo.fail_module.assert_called()
 
     # validate_broker
     def validate_broker_test(self):
@@ -29,7 +36,10 @@ class TestValidateClass(unittest.TestCase):
 
     def validate_broker_fail(self):
         import kafka_topic
-        self.assertRaises(Exception, kafka_topic.validate_broker, broker_definition=[12:9095])
+        mo = mock.Mock()
+        mo.fail_module()
+        broker = validate_broker(broker_definition=[12:9092])
+        mo.fail_module.assert_called()
 
     # validate_ipv4
     def validate_ipv4_test(self):
@@ -39,7 +49,10 @@ class TestValidateClass(unittest.TestCase):
 
     def validate_ipv4_fail(self):
         import kafka_topic
-        self.assertRaises(Exception, kafka_topic.validate_ipv4, broker=[localhost:9095])
+        mo = mock.Mock()
+        mo.fail_module()
+        broker = kafka_topic.validate_ipv4(broker=[localhost:9095])
+        mo.fail_module.assert_called()
 
     # validate_port
     def validate_port_test(self):
@@ -49,7 +62,10 @@ class TestValidateClass(unittest.TestCase):
 
     def validate_port_fail(self):
         import kafka_topic
-        self.assertRaises(Exception, kafka_topic.validate_port, port="70123")
+        mo = mock.Mock()
+        mo.fail_module()
+        port = kafka_topic.validate_port(port="70123")
+        mo.fail_module.assert_called()
 
     # validate_retention_ms
     def validate_retention_ms_test(self):
@@ -59,7 +75,10 @@ class TestValidateClass(unittest.TestCase):
 
     def validate_retention_ms_fail(self):
         import kafka_topic
-        self.assertRaises(Exception, kafka_topic.validate_retention_ms, retention="-7d"
+        mo = mock.Mock()
+        mo.fail_module()
+        retention = kafka_topic.validate_retention_ms(retention="-7d")
+        mo.fail_module.assert_called()
 
 class TestKafkaClass(unittest.TestCase):
 
