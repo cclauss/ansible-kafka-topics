f67097f278f838fc3537554f03058a90d2832124
Johanna Koch <johanna.koch@post.ch>
2019-04-23 14:44:06 +0200

    add shebang, copyright and license

---
+++ kafka_topic.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+# Copyright: (c) 2019, Johanna Koch
+# This software is made available to you under the terms of the Apache2.0 license.
+# Apache License v2.0
+# See LICENSE.txt for details.
