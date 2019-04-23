4bdbb23d04fd4ab79a8527b7460428577ac3aef5
Johanna Koch <johanna.koch@post.ch>
2019-04-23 15:13:50 +0200

    add Documentation

--- kafka_topic.py
+++ kafka_topic.py
@@ -4,3 +4,77 @@
 # This software is made available to you under the terms of the Apache2.0 license.
 # Apache License v2.0
 # See LICENSE.txt for details.
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = '''
+---
+module: kafka_topic
+
+author:
+  - Johanna Koch (@post.ch)
+
+short_description: manage kafka-topics
+
+version_added: "2.7"
+
+description:
+  - create, delete and modify kafka-topics
+
+options:
+  name:
+    description:
+      - Unique name for topic by which it will be identified.
+      - Valid characters are: a-z, A-Z, ".", "-", "_"
+      - Also, the topic must be conform to the ISVC-Topic-Grammar.
+    required: true
+    type: str
+  state:
+    description:
+      - If set to "absent", topic will be deleted if present.
+      - If set to "present", topic will be created if not present.
+    required: true
+    type: str
+    choices: [ absent, present ]
+  partitions:
+    description:
+      - How many partitions are created for the topic.
+      - Partitions can not be set to zero or negative.
+    required: true
+    type: int
+  replication_factor:
+    description:
+      - How many times each partition for the topic is replicated.
+      - The number of replicas can not be more than the number of brokers in the cluster.
+      - Replicas can not be set to zero or negative.
+      - Once the replicas are set, they can not be changed with Ansible.
+    required: true
+    type: int
+  bootstrap_server:
+    description:
+      - Kafka-Broker which is a member of the Kafka-Cluster you want to create the topic on.
+      - Use the following format: "host:port".
+    required: true
+    type: list
+  cleanup_policy:
+    description:
+      - Corresponds to the topic-config "cleanup.policy" from Apache Kafka.
+      - If set to "delete", old segments will be deleted when their retention time or
+        size limits have been reached.
+      - If set to "compact", old segments will be compacted when their retention time
+        or size limits have been reached.
+    default: delete
+    type: str
+    choices: [ delete, compact ]
+  retention:
+    description:
+      - Corresponds to the topic-config "retention.ms" from Apache Kafka.
+      - How long a log will be retained before being discarded.
+      - If set to "-1", no time limit is applied.
+      - Else use the following format: "%d%h%m%s%ms".
+    default: 604800000ms (==7d)
+    type: str
+'''
+
